a09d0b324b806104dd27fd884044ee7c2b42e178
An-Haeeun-02 <101271701+An-Haeeun-02@users.noreply.github.com>
2024-04-17 05:15:40 +0000

    UP

--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -4,14 +4,53 @@
 /**
  * Listing 10.2
  */
-exports.respondWithName = (req, res) => {
+exports.respondWithName1 = (req, res) => {
     res.render('index');
 }
 
 /**
  * Listing 10.3
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
+exports.getHome = (req,res) => {
+    res.render('index'), {
+        name : "AN",
+        people: {
+            usa:{
+                einstein: "Albert Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve jobs"
+            },
+            korea:{
+                psy: "싸이다!",
+                kim: "김수",
+            },
+            mexico:{
+                jesus: "jesus Alvarez"
+            }
+        }
+    }
+    
+    ;} // "/" index
+// exports.getContact = (req,res) => {} // "/contact" GET method
+// exports.postContact = (req,res) => {} // "/contact" POST method
+
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName; //라우트의 매개변수 /name/:myNane
+    res.render('index', { 
+        name: paramsName,
+        people: {
+            usa:{
+                einstein: "Albert Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve jobs"
+            },
+            korea:{
+                psy: "싸이다!",
+                kim: "김민수",
+            },
+            mexico:{
+                jesus: "jesus Alvarez"
+            }
+        }
+    });
 };
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -2,3 +2,34 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+//먼저 모든 상수변수 정리
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+    //2단계 앱 설정(set 함수, 그 다음에 usㄷ함수)
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layouts);
+app.use(express.static("public"));
+
+//get라우트 먼저, 그다음에 post 라우트
+app.get("/", homeController.getHome);
+app.get("/name/:myMane", homeController.respondWithName)
+app.get("/test", (req, res) =>{
+    res.sendFile("./public/test.html");
+});
+
+//순서 때문에 errorController app.ues()은 마지막 listen 전
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound); //404
+app.use(errorController.resInternalError); //500 마지막
+
+    //마지막 listen함수
+app.listen(app.get("port"), () => {
+    console.log(`Server at : http//localhost:${app.get("port")}`)
+});
